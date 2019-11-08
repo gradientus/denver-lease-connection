88c63360374ed7da54649de1f2bbcd6afd145740
Barbara Whiting <whitingba@gmail.com>
2019-11-07 20:22:41 -0700

    Created my initial component and page files

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/Navbar/'
 
 function App() {
