7409af7aafa58e4de80c2bd4cf29a478c037f8b2
Phil Mauracher <gradientus@gmail.com>
2019-11-09 13:50:24 -0700

    Added route to renter application and skeleton renter application form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,20 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Main from './pages/Main';
-import LandlordListings from './pages/LandlordListings';
-import LandlordApplicants from './pages/LandlordApplicants';
-
+import Main from "./pages/Main";
+import LandlordListings from "./pages/LandlordListings";
+import LandlordApplicants from "./pages/LandlordApplicants";
+import RenterApplication from "./pages/RenterApplication";
 
 function App() {
   return (
     <Router>
       <div>
-
-
-
-        <Route exact path='/' component={Main} />
-        <Route exact path='/landlordList' component={LandlordListings} />
-        <Route exact path='/applicants' component={LandlordApplicants} />
-
-
-
-
+        <Route exact path="/" component={Main} />
+        <Route exact path="/landlordList" component={LandlordListings} />
+        <Route exact path="/applicants" component={LandlordApplicants} />
+        <Route exact path="/renterapp" component={RenterApplication} />
       </div>
     </Router>
-
   );
 }
 
