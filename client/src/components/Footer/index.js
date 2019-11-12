8a31c06f137a39e51d372c16f55c5109bc2273ad
Mr. T <c.austin.terry@gmail.com>
2019-11-12 12:50:10 -0700

    Added footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <div className="fixed-bottom">
+      <footer className="footer text-light bg-dark">
+        <container className="container">
+          <div className="row d-flex align-items-center">
+            <div className="col-md-4 col-lg-4 text-center "></div>
+            <div className="col-md-4 col-lg-4 text-center">
+              <i class="far fa-copyright fa-xs"></i>
+              Denver Lease Connection
+            </div>
+
+            <div className="col-md-4 col-lg-4 text-center ">
+              <a
+                className="li-ic"
+                href="https://github.com/gradientus/denver-lease-connection"
+                target="_blank"
+              >
+                <i className="fab fa-github fa-2x text-white"></i>
+              </a>
+            </div>
+          </div>
+
+          {/* <div className="col-md-6 col-lg-7 text-center text-md-right">
+            <a
+              className="li-ic"
+              href="https://github.com/gradientus/denver-lease-connection"
+              target="_blank"
+            >
+              <i className="fab fa-github white-text mr-4"></i>
+            </a>
+          </div> */}
+        </container>
+      </footer>
+    </div>
+  );
+};
+
+export default index;
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import Navbar from '../components/Navbar'
-import GoogleSignIn from '../components/GoogleSignIn';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import GoogleSignIn from "../components/GoogleSignIn";
 
 const Main = () => {
-    return (
-        <div>
-            <Navbar>
-                {/* TODO: dynamically change links based on URL */}
-
-            </Navbar>
-            <GoogleSignIn />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar>{/* TODO: dynamically change links based on URL */}</Navbar>
+      <GoogleSignIn />
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
