f77219a5602219808cfcbf8193160a29a623be58
Mr. T <c.austin.terry@gmail.com>
2019-11-12 21:04:01 -0700

    updated after merge

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -3,29 +3,15 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import GoogleSignIn from "../components/GoogleSignIn";
 
-
-
 const Main = () => {
-<<<<<<< HEAD
   return (
     <div>
       <Navbar>{/* TODO: dynamically change links based on URL */}</Navbar>
+
       <GoogleSignIn />
       <Footer />
     </div>
   );
-=======
-    return (
-        <div>
-            <Navbar>
-                {/* TODO: dynamically change links based on URL */}
-
-            </Navbar>
-
-            <GoogleSignIn />
-        </div>
-    );
->>>>>>> e3fb8d19ed220888ee9042cc7298f893d8083f2f
 };
 
 export default Main;
