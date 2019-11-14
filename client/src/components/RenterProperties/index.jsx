3c861bef37b4dc3501f15041a1d7c2696a14d2fe
Phil Mauracher <gradientus@gmail.com>
2019-11-14 16:19:30 -0700

    Make the renter nav match the landlord nav, add new renterproperties component

---
+++ client/src/components/RenterProperties/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const RenterProperties = () => {
+  return (
+    <div>
+      <h3>O RLY?</h3>
+    </div>
+  );
+};
+
+export default RenterProperties;
