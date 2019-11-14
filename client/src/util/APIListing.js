44dc4a099b7d84e0914a591db535dfa3d710e829
Barbara Whiting <whitingba@gmail.com>
2019-11-14 15:18:55 -0700

    corrected APIlisting.js file

---
+++ client/src/util/APIListing.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+    //function to pull all the listings from the database
+    getListings: function () {
+        return axios.get("/api/listings");
+    },
+    //gunction to get listings by given id
+    getlisting: function (id) {
+        return axios.get("/api/listings/" + id);
+    },
+    //function to delete a book with a given id
+    deleteListing: function (id) {
+        return axios.delete("/api/listings/" + id);
+    },
+    //function to save a book to the database
+    saveListing: function (listingData) {
+        return axios.post("/api/listings", listingData);
+    }
+};
