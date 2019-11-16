9c7dc2ab555460d3ed38deda183435f6bd1f5ef9
Phil Mauracher <gradientus@gmail.com>
2019-11-16 09:49:31 -0700

    Change to .env from keys.js

--- services/passport.js
+++ services/passport.js
@@ -2,54 +2,53 @@ const passport = require("passport");
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
-const keys = require("../config/keys");
+require("dotenv").config();
 
 const User = mongoose.model("user");
 //const User = require('../models/User');
 
 //grabbs the id from the callback function
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
-//find the user in the database by their id 
+//find the user in the database by their id
 passport.deserializeUser((id, done) => {
-    User.findById(id).then(user => {
-        done(null, user);
-    });
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.googleClientId,
-            clientSecret: keys.googleClientSecret,
-            callbackURL: "/auth/google/callback",
-            proxy: true
-        },
+  new GoogleStrategy(
+    {
+      clientID: process.env.googleClientId,
+      clientSecret: process.env.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
 
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then(existingUser => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    //create a new user if they do not already exist in our database
-                    new User({
-                        googleId: profile.id,
-                        firstName: profile.name.givenName,
-                        lastName: profile.name.familyName
-                    })
-                        .save()
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          //create a new user if they do not already exist in our database
+          new User({
+            googleId: profile.id,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName
+          })
+            .save()
 
-
-                        .then((user) => {
-                            console.log('new user created: ' + user);
-                            done(null, user);
-                        })
-                }
+            .then(user => {
+              console.log("new user created: " + user);
+              done(null, user);
             });
-
-            //console.log("profile", profile);
         }
-    )
+      });
+
+      //console.log("profile", profile);
+    }
+  )
 );
