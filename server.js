02f8899ed53a36f7dfebdeec6e74f6ee92a49dd7
Phil Mauracher <gradientus@gmail.com>
2019-11-16 14:27:12 -0700

    undo the wrong stuff

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
+// app.get("*", (request, response) => {
+//   response.sendFile(path.join(__dirname, "client/build", "index.html"));
+// });
 
 //Routes
 app.use("/api/listings", listings);
