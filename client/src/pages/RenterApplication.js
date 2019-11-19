33b8baf9983891b8ece28da72c6af06f355d6e25
Phil Mauracher <gradientus@gmail.com>
2019-11-19 15:55:12 -0700

    Recover from changes that cause errors all over the place.  Have state set in renter applicalication, and fixed some problems with API.

--- client/src/pages/RenterApplication.js
+++ client/src/pages/RenterApplication.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import ApplicationForm from "../components/ApplicationForm";
+//import API from "../util/API";
 import Footer from "../components/Footer";
 import { Col, Row } from "reactstrap";
 
-const RenterApplication = () => {
-  var firstName = "Phil";
-  var lastName = "Smith";
-  var state = "Georgia";
-  var ip = process.env.ip;
-  var timeStamp = Math.round(new Date().getTime() / 1000);
+class RenterApplication extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+    phone: "",
+    email: "",
+    employment: "",
+    income: "",
+    info: ""
+  };
 
-  return (
-    <div>
-      <ApplicationForm />
-      <Row>&nbsp; </Row>
-      <Row>&nbsp; </Row>
-      <Row>&nbsp;</Row>
-      <Row>&nbsp; </Row>
-      <Row>&nbsp; </Row>
-      <Footer />
-    </div>
-  );
-};
+  //QUESTION: Do I need to make a call to the userAPI? To match it with the application?  Is that needed?
+  //TODO: I do need to load the API to post the form information to the collection
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ApplicationForm />
+        <Row>&nbsp; </Row>
+        <Row>&nbsp; </Row>
+        <Row>&nbsp;</Row>
+        <Row>&nbsp; </Row>
+        <Row>&nbsp; </Row>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default RenterApplication;
--- client/src/util/API.js
+++ client/src/util/API.js
@@ -3,7 +3,7 @@ import info from "../pages/RenterApplication";
 require("dotenv").config();
 
 export default {
-  getInmate: function() {
+  getInmate: function(APP_ID, APP_KEY, timeStamp, ip, firstName, lastName) {
     //return a promise object
     return axios.get(
       `http://apijson.backgroundcheckapi.com/?App_ID=${process.env.APP_ID}&App_Key=${process.env.APP_KEY}&Timestamp=${info.timeStamp}&IP=${info.ip}&catalogue=BACKGROUND&FirstName=${info.firstName}&LastName=${info.lastName}&MiddleName=&State=${info.state}&BirthYear=&CrimeType=&ExactMatch=Yes`
